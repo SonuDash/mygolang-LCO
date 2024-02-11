fc54a51fd83316505cf6d4b9e4a4a1007d939959
Sonu Dash <72654041+SonuDash@users.noreply.github.com>
2024-02-11 16:26:36 +0530

    modified

--- 19handlingurls/main.go
+++ 19handlingurls/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghbj456ghb"
 
 func main() {
 	fmt.Println("Handling URLs in Go")
+	fmt.Println(myurl)
+
+	//parsing
+	result, _ := url.Parse(myurl)
+	// fmt.Println(result.Scheme)
+	// fmt.Println(result.Host)
+	// fmt.Println(result.Path)
+	// fmt.Println(result.Port())
+	fmt.Println(result.RawQuery)
+
+	qparams := result.Query()
+	fmt.Printf("The type of query params are: %T\n", qparams)
+	fmt.Println(qparams["coursename"])
+
+	for _, val := range qparams {
+		fmt.Println("Param is: ", val)
+	}
+	partsofUrl := &url.URL{
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
+		RawPath: "user=hitesh",
+	}
+
+	anotherURL := partsofUrl.String()
+	fmt.Println(anotherURL)
 }
